839528e52554f04f207352db97f2d321b0efd145
Emanuele Bezzi <bezzi@datastampaweb.it>
2016-01-27 18:11:55 +0100

    problems

---
+++ strings/gemstones.hs
@@ -0,0 +1,17 @@
+import Data.List
+
+getMultipleLines :: Int -> IO [String]
+getMultipleLines n
+    | n <= 0 = return []
+    | otherwise = do          
+        x <- getLine         
+        xs <- getMultipleLines (n-1)    
+        return (x:xs)            
+
+main :: IO ()
+main = do
+    n_temp <- getLine
+    let n = read n_temp :: Int
+    xs <- getMultipleLines n
+    let res = foldl intersect (head xs) xs
+    print $ length res
